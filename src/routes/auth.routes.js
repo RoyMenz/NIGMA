ac5f4ca93d46345475fd12f1c9d95bf8f3948963
Roy Creatives <roymenezes28@gmail.com>
2026-02-05 12:09:25 +0530

    Delete src directory

--- src/routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { signupLeader } = require("../controllers/auth.controller");
-
-router.post("/signup", signupLeader);
-
-module.exports = router;
