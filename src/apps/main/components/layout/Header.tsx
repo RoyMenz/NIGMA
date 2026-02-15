114124e83e9b2c00e83e1e043b21708a41ef8dd1
ronitover <jronith39@gmail.com>
2026-02-15 14:50:05 +0530

    Renamed Header Hackathon button

--- src/apps/main/components/layout/Header.tsx
+++ src/apps/main/components/layout/Header.tsx
@@ -17,7 +17,7 @@ const Header: React.FC = () => {
         </div>
         <div className="landing-nav-right">
           <button className="landing-hackathon-btn" onClick={openHackathon}>
-            RagnoCode
+            Hackathon
           </button>
         </div>
       </div>
